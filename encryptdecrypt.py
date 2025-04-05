416bf5bc46705f3431e7a2ca979d315992e10682
Abhishek <abhishekkhanaj007@gmail.com>
2025-04-05 15:56:04 +0530

    commiting from UK

--- encryptdecrypt.py
+++ encryptdecrypt.py
@@ -4,7 +4,7 @@ alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 'a'
 ]
-
+print("I am commiting from UK")
 direction = input("type 'encode' to encrypt, type 'decode' to decrypt \n").lower()
 text = input("type your message \n").lower()
 shift = int(input("type shift nuber \n"))
