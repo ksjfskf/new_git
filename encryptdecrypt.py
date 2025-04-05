80b4e4293af6670cc375ff240b45452aa67f408c
Abhishek <abhishekkhanaj007@gmail.com>
2025-04-05 15:54:10 +0530

    added one more variable

--- encryptdecrypt.py
+++ encryptdecrypt.py
@@ -2,7 +2,7 @@
 
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 'a'
 ]
 
 direction = input("type 'encode' to encrypt, type 'decode' to decrypt \n").lower()
